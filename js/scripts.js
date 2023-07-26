dc925e5fee8060ec5c2dc04edc7fb3313f79440f
arshdbrar <139406297+arshdbrar@users.noreply.github.com>
2023-07-26 10:56:20 -0700

    created pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-alert('Hello World');
-let favoriteFood = 'sushi';
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Charizard',
+        height: 1.7,
+        types: ['fire', 'flying']
+    },
+    {
+        name: 'Pikachu',
+        height: 0.4,
+        types: 'electric'
+    },
+    {
+        name: 'Gyarados',
+        height: 6.5,
+        types: ['water', 'flying']
+    }
+];
