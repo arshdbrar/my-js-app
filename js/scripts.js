2ec0645e6d15648b425e6f8dc180eddedc25f269
arshdbrar <139406297+arshdbrar@users.noreply.github.com>
2023-07-28 09:43:26 -0700

    conditional statement added

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,12 @@ let pokemonList = [
         types: ['water', 'flying']
     }
 ];
+
+//used for function to display Pokemon name and corresponding height
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write("<p>" + pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")");
+    //conditional statement added to signify special property of Pokemon over height 2
+    if (pokemonList[i].height > 2) {
+        document.write(" " + "-" + " " + "Wow, that's big!");
+    }
+}
