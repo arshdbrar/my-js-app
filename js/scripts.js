4c0de1dbc3b88411a1008ec451473363859d5031
arshdbrar <139406297+arshdbrar@users.noreply.github.com>
2023-08-02 15:05:47 -0700

    repository and foreach function added

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,36 @@
-let pokemonList = [
-    {
-        name: 'Charizard',
-        height: 1.7,
-        types: ['fire', 'flying']
-    },
-    {
-        name: 'Pikachu',
-        height: 0.4,
-        types: ['electric']
-    },
-    {
-        name: 'Gyarados',
-        height: 6.5,
-        types: ['water', 'flying']
-    }
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Charizard',
+            height: 1.7,
+            types: ['fire', 'flying']
+        },
+        {
+            name: 'Pikachu',
+            height: 0.4,
+            types: ['electric']
+        },
+        {
+            name: 'Gyarados',
+            height: 6.5,
+            types: ['water', 'flying']
+        }
+    ];
 
-//used for function to display Pokemon name and corresponding height
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write("<p>" + pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")");
-    //conditional statement added to signify special property of Pokemon over height 2
-    if (pokemonList[i].height > 2) {
-        document.write(" " + "-" + " " + "Wow, that's big!");
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-}
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ")")
+});
